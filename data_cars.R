8464509042631d1527fdd39be4b1a8c7c800db6b
Lintang Djenar <lintangdjenar2019@gmail.com>
2026-05-14 13:14:28 +0700

    Update: Menambahkan analisis regresi linear dan visualisasi estetik pada data_cars.R

--- data_cars.R
+++ data_cars.R
@@ -11,3 +11,22 @@ for (i in 1:n){
 }
 
 hist(mtcars$mpg)
+
+# Menambahkan hasil kategori ke data asli
+mtcars_final <- cbind(mtcars, Kategori = status)
+
+# Analisis Regresi
+# Melihat pengaruh Berat Mobil (wt) terhadap MPG
+model_regresi <- lm(mpg ~ wt, data = mtcars)
+summary(model_regresi)
+
+plot(mtcars$wt, mtcars$mpg, 
+     main = "Analisis Regresi: Berat Mobil vs Efisiensi",
+     xlab = "Berat Mobil (1000 lbs)", 
+     ylab = "Miles Per Gallon (MPG)",
+     pch = 19, 
+     col = "hotpink", 
+     frame.plot = FALSE)
+
+# Menambahkan garis regresi
+abline(model_regresi, col = "red", lwd = 2)
